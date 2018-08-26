466741431be9ba1afb33f9e0a2003ac1cf587917
hr82al <hr82al@gmail.com>
2018-08-26 09:25:04 +0300

    В main добавлен метод sum, суммирующий все эелементы массива.

---
+++ src/main/java/ru/geekbrains/java2/lesson_2/SumArray.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.java2.lesson_2;
+
+public class SumArray {
+    public static void main(String[] args) {
+        String[][] validArray = new String[][]
+                {{"56", "32", "1", "32"},
+                 {"12", "18", "11", "98"},
+                 {"14", "14", "8", "18"},
+                 {"15", "56", "15", "91"}};
+
+        System.out.println(sum(validArray));
+    }
+
+    private static long sum(String[][] validArray) {
+        //TODO FIXME
+        return 0;
+    }
+
+}
