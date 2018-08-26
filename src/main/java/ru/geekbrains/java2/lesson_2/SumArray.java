7e56e0974bbade48f25eaf9498a7837094da738d
hr82al <hr82al@gmail.com>
2018-08-26 10:08:48 +0300

    Добавленно исключение проверяющее корректность данных в массиве.

--- src/main/java/ru/geekbrains/java2/lesson_2/SumArray.java
+++ src/main/java/ru/geekbrains/java2/lesson_2/SumArray.java
@@ -3,11 +3,12 @@ package ru.geekbrains.java2.lesson_2;
 public class SumArray {
     private static final int ARRAY_SISE = 4;
     private static final String WRONG_SIZE_ARRAY = "Неправильный размер массива!\nОдидаемый размер: 4x4.";
+    private static final String WRONG_DATA_ARRAY = "Неверный тип дынных в массиве.";
 
     public static void main(String[] args) {
         String[][] validArray = new String[][]
                 {{"56", "32", "1", "32"},
-                 {"12", "18", "11", "98"},
+                 {"12", "18", "19", "98"},
                  {"14", "14", "8", "18"},
                  {"15", "56", "15", "91"}};
 
@@ -20,8 +21,23 @@ public class SumArray {
 
     private static long sum(String[][] validArray) throws MyArrayExceptions{
         checkSizeArray(validArray);
+        checkIntsArray(validArray);
         //TODO FIXME
         return 0;
+    }
+
+    private static void checkIntsArray(String[][] _array) throws MyArrayDataException{
+        int row;
+        int col;
+        for (row = 0; row < _array.length; row++) {
+            for (col = 0; col < _array[row].length; col++) {
+                try {
+                    Integer.parseInt(_array[row][col]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(WRONG_DATA_ARRAY);
+                }
+            }
+        }
     }
 
     private static void checkSizeArray(String[][] _array) throws MyArraySizeException{
