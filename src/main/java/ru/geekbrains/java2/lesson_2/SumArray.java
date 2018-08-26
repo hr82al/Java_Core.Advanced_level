cd97272de1bf121a096a72f5b48109c393acd3e0
hr82al <hr82al@gmail.com>
2018-08-26 09:55:21 +0300

    Добавлено исключение, проверяющее правильность размера массива.

--- src/main/java/ru/geekbrains/java2/lesson_2/SumArray.java
+++ src/main/java/ru/geekbrains/java2/lesson_2/SumArray.java
@@ -1,6 +1,9 @@
 package ru.geekbrains.java2.lesson_2;
 
 public class SumArray {
+    private static final int ARRAY_SISE = 4;
+    private static final String WRONG_SIZE_ARRAY = "Неправильный размер массива!\nОдидаемый размер: 4x4.";
+
     public static void main(String[] args) {
         String[][] validArray = new String[][]
                 {{"56", "32", "1", "32"},
@@ -8,12 +11,28 @@ public class SumArray {
                  {"14", "14", "8", "18"},
                  {"15", "56", "15", "91"}};
 
-        System.out.println(sum(validArray));
+        try {
+            System.out.println(sum(validArray));
+        } catch (MyArrayExceptions myArrayExceptions) {
+            myArrayExceptions.printStackTrace();
+        }
     }
 
-    private static long sum(String[][] validArray) {
+    private static long sum(String[][] validArray) throws MyArrayExceptions{
+        checkSizeArray(validArray);
         //TODO FIXME
         return 0;
+    }
+
+    private static void checkSizeArray(String[][] _array) throws MyArraySizeException{
+        if (_array.length != ARRAY_SISE) {
+            throw new MyArraySizeException(WRONG_SIZE_ARRAY);
+        }
+        for (String[] strings : _array) {
+            if (strings.length != ARRAY_SISE) {
+                throw new MyArraySizeException(WRONG_SIZE_ARRAY);
+            }
+        }
     }
 
 }
